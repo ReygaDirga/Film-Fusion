4fdb953ec9341de6bc513d610b7e62a7be369d38
ReygaDirga <reygadirg@gmail.com>
2023-10-24 14:06:37 +0700

    update toprated

--- src/pages/toprated.jsx
+++ src/pages/toprated.jsx
@@ -1,39 +1,86 @@
-import {getTopList} from "../api/api"
-import { useEffect, useState } from 'react';
 import '../App.css';
+import {useEffect, useState} from "react"
+import axios from 'axios';
 
-const toprated = () => {
+const Toprated = () => {
 
-    // eslint-disable-next-line react-hooks/rules-of-hooks
     const [topRated, setTopRated] = useState([])
-
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    useEffect(() => {
-        getTopList().then((result) => {
-        setTopRated(result)
-        })
+    
+    useEffect(()=>{
+        getTop();
     }, [])
 
-    const TopRatedMovieList = () => {
-        return topRated.map((movie, i) => {
-        return(
-            <div className="Movie-wrapper" key={i}>
-                <div className="Movie-title">{movie.title}</div>
-                <img className="Movie-image" src={`${process.env.REACT_APP_BASEIMG}/${movie.poster_path}`} alt=""/>
-                <div className="Movie-date">Release : {movie.release_date}</div>
-                </div>
-        )
-        })
-    }
+    const getTop = async () => {
+        try {
+          const response = await axios.get(
+            `${process.env.REACT_APP_BASEURL}/movie/top_rated?page=1&api_key=${process.env.REACT_APP_APIKEY}`
+          );
+          const data = response.data.results;
+          setTopRated(data);
+        } catch (error) {
+          console.log("API NOT CALLED");
+        }
+      };
 
     return(
-    <div className="App">
-      <header className="App-header">
+<>
+      <div className="App">
+        <div className="App-header">
           <div className="Movie-container">
-              <TopRatedMovieList/>
+            {topRated.map((movie, i) => (
+              <div className="Movie-wrapper" key={i}>
+                <div className="Movie-title">{movie.title}</div>
+                <img
+                  className="Movie-image"
+                  src={`${process.env.REACT_APP_BASEIMG}/${movie.poster_path}`}
+                  alt=" "
+                />
+                <div className="Movie-date">Release : {movie.release_date}</div>
+              </div>
+            ))}
           </div>
-      </header>
-    </div>
+        </div>
+      </div>
+    </>
     )
 }
-export default toprated;
+export default Toprated;
+
+
+
+
+// const toprated = () => {
+
+//     // eslint-disable-next-line react-hooks/rules-of-hooks
+//     const [topRated, setTopRated] = useState([])
+
+//     // eslint-disable-next-line react-hooks/rules-of-hooks
+//     useEffect(() => {
+//         getTopList().then((result) => {
+//         setTopRated(result)
+//         })
+//     }, [])
+
+//     const TopRatedMovieList = () => {
+//         return topRated.map((movie, i) => {
+//         return(
+//             <div className="Movie-wrapper" key={i}>
+//                 <div className="Movie-title">{movie.title}</div>
+//                 <img className="Movie-image" src={`${process.env.REACT_APP_BASEIMG}/${movie.poster_path}`} alt=""/>
+//                 <div className="Movie-date">Release : {movie.release_date}</div>
+//                 </div>
+//         )
+//         })
+//     }
+
+//     return(
+//     <div className="App">
+//       <header className="App-header">
+//           <div className="Movie-container">
+//               <TopRatedMovieList/>
+//           </div>
+//       </header>
+//     </div>
+//     )
+// }
+// export default toprated;
